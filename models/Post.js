0aa0415b84476ef7e61f7f14ecb81c48d1e960a4
FDeaquino <fidel.deaquino@gmail.com>
2022-11-06 13:58:53 -0600

    created the Vote model, created a PUT route for voting on a post, updated GET routes to include votes, refactoring

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,32 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model { }
+class Post extends Model {
+    // with this upvote method, we pass in the value of req.body as body, and an object of the models as models
+    // passes the Vote model as an argument from post-routes.js
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 
 // create fields/columns for Post model
 // configure the naming conventions and pass the current connection instance to initialize the Post model
--- models/index.js
+++ models/index.js
@@ -7,6 +7,9 @@ const User = require('./User');
 // imports the Post model
 const Post = require('./Post');
 
+// imports the Vote model
+const Vote = require('./Vote');
+
 // create model associations
 // ONE TO MANY relationship (one user can have many posts)
 User.hasMany(Post, {
@@ -18,5 +21,43 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+// MANY TO MANY Relationship
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+// MANY TO MANY Relationship
+Post.belongsToMany(User, {
+through: Vote,
+as: 'voted_posts',
+foreignKey: 'post_id'
+});
+
+// Direct relationship between vote and user
+// Allows us to see ?? who the vote belongs to
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+// Direct relationship between vote and post
+// Allows us to see the total count of votes for a single post when queried
+Vote.belongsTo(Post, {
+foreignKey: 'post_id'
+});
+
+// One to Many relationship
+User.hasMany(Vote, {
+foreignKey: 'user_id'
+});
+
+// One to Many relationship
+Post.hasMany(Vote, {
+foreignKey: 'post_id'
+});
+  
+
 // exports an object with it as a property
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
