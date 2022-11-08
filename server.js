4ff4a7801cff08be716c272c4ae44d756beb08de
FDeaquino <fidel.deaquino@gmail.com>
2022-11-08 14:15:15 -0600

    set up the project, set up the template engine, created the homepage template, populated the template with Sequelize data

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 // The router instance in routes/index.js collects everything and packages them up for server.js to use
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+// express handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Express.js middleware that makes the contents of this folder and serves them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// express handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
