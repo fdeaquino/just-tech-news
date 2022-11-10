19426ec0bd8ef4ac772b89c19fb6fedc8ce2f84d
FDeaquino <fidel.deaquino@gmail.com>
2022-11-10 17:19:42 -0600

    created a login page, created a session on the back end, added front-end login to forms, added logic to destroy the session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,14 +27,14 @@ router.get('/:id', (req, res) => {
         },
         include: [
             {
-              model: Post,
-              attributes: ['id', 'title', 'post_url', 'created_at']
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
             },
             {
-              model: Post,
-              attributes: ['title'],
-              through: Vote,
-              as: 'voted_posts'
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
             }
         ]
     })
@@ -60,10 +60,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+        // allows us to access the session information in the route
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
         });
 });
 
@@ -89,9 +94,26 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
--- server.js
+++ server.js
@@ -3,13 +3,29 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
-
-// express handlebars
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// express handlebars
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
